57b2fb13122e0be2229c943bd030ac8f78cc2f23
Pantelis Manikas <pmanikas.social@gmail.com>
2018-11-04 04:20:20 +0200

    make avatar component

--- src/partials/About.vue
+++ src/partials/About.vue
@@ -3,9 +3,10 @@
     .row.fullHeight
       .jar-md-75.jar-md-push-25.profile-details
         .baseContent
-          Avatar
-          .profile-text
-            p {{owner.description}}
+          .profileInfo(v-inViewport.once.fully)
+            Avatar
+            .profileText
+              p {{owner.description}}
           Steps
           BaseVerLine(:height="'90vh'" :widthy="'10px'" :bottom="'0'" :left="'0'")
           BaseVerLine(:height="'50vh'" :bottom="'0'" :right="'0'")
@@ -16,12 +17,16 @@
 import { owner } from '@data/data'
 import Avatar from '@components/Avatar'
 import Steps from '@components/Steps'
+import inViewportDirective from 'vue-in-viewport-directive'
 
 export default {
   name: 'About',
   components: {
     Avatar,
     Steps
+  },
+  directives: {
+    inViewport: inViewportDirective
   },
   data() {
     return {
@@ -30,3 +35,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.profileInfo
+  transition: transform 1s ease-in-out
+  transform: scale(0)
+  &.fully-in-viewport
+    transform: scale(1)
+</style>
