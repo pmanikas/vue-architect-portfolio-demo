abee25116d0511cbf89e11ea98de0527334ba7fe
Pantelis Manikas <pmanikas.social@gmail.com>
2018-11-08 15:30:55 +0200

    improve router scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,9 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes,
   mode: 'history',
-  // scrollBehavior() {
-  //   setTimeout(() => {
-  //     return window.scrollTo({ top: 0 })
-  //   }, 10)
-  // },
   scrollBehavior(to, from, savedPosition) {
     setTimeout(() => {
-      if (savedPosition) {
-        return savedPosition
-      } else {
-        return window.scrollTo({ top: 0 })
-      }
+      return window.scrollTo({ top: 0 })
     }, 10)
   },
 })
