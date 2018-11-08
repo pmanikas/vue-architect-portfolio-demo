5cf595a510319c0f42d8bfec12b8faf7cc2c5cda
Pantelis Manikas <pmanikas.social@gmail.com>
2018-11-08 16:01:03 +0200

    fill 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,49 @@
-<template>
-  <h1>404 Not Found</h1>
+<template lang="pug">
+  .contactPage
+    .container
+      .row
+        .jar-xs-100
+          h1 Page Not Found
+      .row
+        .jar-xs-100
+          .bigUglyLetters 404
+      .row
+        .buttons
+          .button
+            router-link(to="/") 
+              BaseButton(:text="'Go Back to Home Page'")
+
+
 </template>
 
 <script>
 export default {
-  name: '404'
+  name: 'NotFound',
+  metaInfo: {
+    title: '404 | Page not Found'
+  },
 }
 </script>
+
+<style lang="sass" scoped>
+@import '~@assets/style/essentials'
+
+.bigUglyLetters
+  font-size: 20rem
+  text-align: center
+  opacity: 0.5
+
+.buttons
+  text-align: center
+.button
+  margin-bottom: $base-spacing
+  vertical-align: middle
+
+@media(min-width: $desktop)
+  .buttons
+    text-align: left
+  .button
+    display: inline-block
+  .button~.button
+    margin-left: $base-spacing
+</style>
