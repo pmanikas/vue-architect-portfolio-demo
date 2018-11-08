4b930a4d88711a88ee888d8a7f120893f29676e7
Pantelis Manikas <pmanikas.social@gmail.com>
2018-11-08 16:07:10 +0200

    change smudge styling to ove it from header to about section

--- src/components/Smudge.vue
+++ src/components/Smudge.vue
@@ -11,13 +11,13 @@ export default {
 <style lang="sass" scoped>
   .smudge
     position: absolute
+    top: 0
     right: 0
-    bottom: 0
     width: 550px
     height: 720px
     background: url(~@/assets/img/smudge.png) no-repeat
     background-position: center
     background-size: contain
     opacity: 0.3
-    transform: translateY(50%)
+    transform: translateY(-50%)
 </style>
