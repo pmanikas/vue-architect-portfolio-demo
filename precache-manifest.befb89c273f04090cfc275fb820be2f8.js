a49a05bd17216a13717152b10fa0bd8c8d6aad71
Pantelis Manikas <pmanikas.social@gmail.com>
2021-03-29 16:09:02 +0300

    Updates

---
+++ precache-manifest.befb89c273f04090cfc275fb820be2f8.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5a793e93a423bc6af3a3d7a8909b50",
+    "url": "/vue-architect-portfolio-demo/.htaccess"
+  },
+  {
+    "revision": "a65cf142a9e7158bff24",
+    "url": "/vue-architect-portfolio-demo/css/app.c0e52076.css"
+  },
+  {
+    "revision": "d6edbf6d4b5d1771b0cd6bec3c4b7406",
+    "url": "/vue-architect-portfolio-demo/fonts/martha.d6edbf6d.ttf"
+  },
+  {
+    "revision": "1f21791bd74447a28255368d8c10f454",
+    "url": "/vue-architect-portfolio-demo/img/arrow-back.1f21791b.png"
+  },
+  {
+    "revision": "1dc61235a0db07f30ffc980efcd1de8c",
+    "url": "/vue-architect-portfolio-demo/img/bench.1dc61235.png"
+  },
+  {
+    "revision": "15f365e3b35e8a7fdf07ab13f0dbc7b3",
+    "url": "/vue-architect-portfolio-demo/img/benchman.15f365e3.png"
+  },
+  {
+    "revision": "d749c81cca02553a4180d0f50f9c2854",
+    "url": "/vue-architect-portfolio-demo/img/blackbord-bg.d749c81c.jpg"
+  },
+  {
+    "revision": "87f1bb93e2c4209b2e46b4d61ba96332",
+    "url": "/vue-architect-portfolio-demo/img/border.87f1bb93.png"
+  },
+  {
+    "revision": "9efc82bed3ba6275ac09098011057948",
+    "url": "/vue-architect-portfolio-demo/img/bottom-left-smudge.9efc82be.png"
+  },
+  {
+    "revision": "810cd5f4c3fced62d5705a02e560fb10",
+    "url": "/vue-architect-portfolio-demo/img/bush.810cd5f4.png"
+  },
+  {
+    "revision": "d4e7cfacd898f0d0fd8fd86998429c5b",
+    "url": "/vue-architect-portfolio-demo/img/compass.d4e7cfac.png"
+  },
+  {
+    "revision": "218b301799a546160a2847fed7fa5738",
+    "url": "/vue-architect-portfolio-demo/img/doggyman.218b3017.png"
+  },
+  {
+    "revision": "fb1c652dc6439a86e7ed9c00eb08b47a",
+    "url": "/vue-architect-portfolio-demo/img/double-bush.fb1c652d.png"
+  },
+  {
+    "revision": "eb5c036a39163c5a4134eb2442ecd5de",
+    "url": "/vue-architect-portfolio-demo/img/face.eb5c036a.png"
+  },
+  {
+    "revision": "72dfb81e96d673d05afd758e3475c903",
+    "url": "/vue-architect-portfolio-demo/img/grass.72dfb81e.png"
+  },
+  {
+    "revision": "3da5e3665a2bf741c08a780ca959e621",
+    "url": "/vue-architect-portfolio-demo/img/horizontal-line.3da5e366.png"
+  },
+  {
+    "revision": "92b38409e92e45bffe48e4de93441723",
+    "url": "/vue-architect-portfolio-demo/img/image-not-found.92b38409.jpg"
+  },
+  {
+    "revision": "a9a149af415f22157cc9545180613401",
+    "url": "/vue-architect-portfolio-demo/img/light.a9a149af.png"
+  },
+  {
+    "revision": "de385ebe0314f4557f479731eb5e2739",
+    "url": "/vue-architect-portfolio-demo/img/sea.de385ebe.png"
+  },
+  {
+    "revision": "9500b92b37b1b5bfe86938d56543dd18",
+    "url": "/vue-architect-portfolio-demo/img/seaman.9500b92b.png"
+  },
+  {
+    "revision": "3092e5b1e29cb4ff1d0745de9c4ec5e2",
+    "url": "/vue-architect-portfolio-demo/img/smudge.3092e5b1.png"
+  },
+  {
+    "revision": "0916bb0545fd7d9894b092c186994a48",
+    "url": "/vue-architect-portfolio-demo/img/tree.0916bb05.png"
+  },
+  {
+    "revision": "ac115b67fcd472948d2d7975dd5cda42",
+    "url": "/vue-architect-portfolio-demo/img/vertical-line.ac115b67.png"
+  },
+  {
+    "revision": "9c4ff6ce9ba24c15884464fb481f9cb8",
+    "url": "/vue-architect-portfolio-demo/index.html"
+  },
+  {
+    "revision": "a65cf142a9e7158bff24",
+    "url": "/vue-architect-portfolio-demo/js/app.341f5a1c.js"
+  },
+  {
+    "revision": "5aa17e2a4e38ef33aa64",
+    "url": "/vue-architect-portfolio-demo/js/chunk-vendors.a9ee9eea.js"
+  },
+  {
+    "revision": "c652f3bab61e66e233d3d23f56f0cf06",
+    "url": "/vue-architect-portfolio-demo/maily/.htaccess"
+  },
+  {
+    "revision": "57fd2ff3e3c6fbb89234a4a736465410",
+    "url": "/vue-architect-portfolio-demo/maily/mail-dev.php"
+  },
+  {
+    "revision": "5c949004c473031a4fe6414def4b1f7e",
+    "url": "/vue-architect-portfolio-demo/maily/mail.php"
+  },
+  {
+    "revision": "bcc2c90f78eb67233e201f19e8334b88",
+    "url": "/vue-architect-portfolio-demo/manifest.json"
+  },
+  {
+    "revision": "d3087b14b5dc8dce0f8cb6e07d107d29",
+    "url": "/vue-architect-portfolio-demo/robots.txt"
+  }
+]);
